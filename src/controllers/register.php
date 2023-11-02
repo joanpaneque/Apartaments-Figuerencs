380139e4b73ff518cf9170ca6bc06a8eca9c8ff1
joan <jpaneque@cendrassos.net>
2023-11-02 16:45:13 +0100

    Register funcitionality added

--- src/controllers/register.php
+++ src/controllers/register.php
@@ -1,5 +1,32 @@
 <?php
     function ctrlRegister ($request, $response, $container) {
-        $response->setTemplate("register.php");
+
+        $registering = $request->get(INPUT_GET, "dologin");
+
+        $name = $request->get(INPUT_GET, "name");
+        $surname = $request->get(INPUT_GET, "surname");
+        $phone = $request->get(INPUT_GET, "phone");
+        $email = $request->get(INPUT_GET, "email");
+        $password = $request->get(INPUT_GET, "password");
+
+        if (!$registering) {
+            $response->setTemplate("register.php");
+            return $response;
+        }
+
+        $errorMessage = "";
+
+        $success = $container->user()->register($name, $surname, $email, $phone, $password);
+
+        if ($success) {
+            $response->redirect("location: index.php");
+            return $response;
+        } else {
+            $errorMessage = "No s'ha pogut registrar l'usuari";
+            echo $errorMessage;
+            $response->setTemplate("register.php");
+        }
+
+
         return $response;
     }
